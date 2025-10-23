c46b2ee21fc9f1fd11c5e4e5f5ea0f0dd71ab6f5
sphynkx <sfynkx@gmail.com>
2025-10-23 17:06:14 +0300

    Remove test buttons from main menu.

--- menus/main_menu.py
+++ menus/main_menu.py
@@ -5,11 +5,6 @@ from i18n.messages import tr
 def main_reply_keyboard(msg=None):
     kb = [
         [
-            KeyboardButton(text=tr("main_menu.test1", msg=msg)),
-            KeyboardButton(text=tr("main_menu.test2", msg=msg)),
-            KeyboardButton(text=tr("main_menu.test3", msg=msg))
-        ],
-        [
             KeyboardButton(text=tr("main_menu.language", msg=msg)),
             KeyboardButton(text=tr("main_menu.korneslov", msg=msg))
         ],
